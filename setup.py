a9db74b73f4f7d50fa390762e76aa7e990dd47f1
Jakub Markowiak <jamarkowiak@gmail.com>
2022-08-25 15:07:56 +0200

    modify setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ except ImportError:
 
 setup(
     include_package_data=True,
-    name='Geohash tools',
-    version='0.1',
+    name='geohash_tools',
+    version='0.1.1',
     license='MIT',
     description='Geohash tools for performing geohash related tasks.',
     url="https://github.com/Jakub-Markowiak/geohash-tools",
+    download_url="https://github.com/Jakub-Markowiak/geohash-tools/archive/refs/tags/v0.1.tar.gz",
     author='Jakub Markowiak',
     author_email='jamarkowiak@gmail.com',
     packages=find_packages(),
-    install_requires=['math', 'sys', 'unittest'],
     long_description='This module provides optimized functions to perform geohash related tasks. Module includes encoding and decoding geohashes, finding neighbouring geohashes and calculating exact distance between two geohashes using haversine formula',
     long_description_content_type="text/markdown",
     classifiers = [
